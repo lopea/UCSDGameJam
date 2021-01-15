19132f2845d87c90d0a450b051239d7301053cb1
Javier Sandoval <Javiers2001@live.com>
2021-01-14 23:45:06 -0600

    Added My Scene + GitHub Support

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
+    
+    [SerializeField]
+    float _Speed = 5.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,9 @@ public class FirstPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        transform.position += input * Time.deltaTime * _Speed;
+      
     }
 }

de772cf37f813d0cb62f3fb5a2391819d577bc89
mwalker8029 <mwalker8029@gmail.com>
2021-01-16 17:11:14 -0800

    gg
    gg

--- Assets/Global/Scripts/GlobalVariableStorage.cs
+++ Assets/Global/Scripts/GlobalVariableStorage.cs
@@ -9,9 +9,6 @@ public class GlobalVariableStorage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ShotgunOffset.x = 0;
-        ShotgunOffset.y = 0;
-        ShotgunOffset.z = 0;
         EnemyTargetThePlayer = transform;
         EnemyShotgunOffset = transform.position;
     }
